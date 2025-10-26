326fe353cb85d8298962d24b52e043a00d079aca
Clasid Vegatea <yashodakunni@outlook.com>
2025-10-26 07:40:48 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 # Define long image names with readable labels
-IMAGE1="ghcr.io/hrdwasnetskydockim0539xzref/newdocvhh65hvwbjvj87hchf42j768vjht542bj84bjbitanetskey0539:001-of-one"
-IMAGE2="ghcr.io/hrdwasnetskydockim0539xzref/newdocvhh65hvwbjvj87hchf42j768vjht542bj84bjbitanetskey0539:001-of-one"
-IMAGE3="ghcr.io/hrdwasnetskydockim0539xzref/newdocvhh65hvwbjvj87hchf42j768vjht542bj84bjbitcnetskey0539:001-of-three"
+IMAGE1="ghcr.io/nixegohshs5f4ef6vuun43f6/iaw4sm7rs56v3cy7rv66v4rfgu6yu843dawsmsrbitapyv3125:yhf3gj79bt8kytv57vttv7awsmsrbitA0001-of-one"
+IMAGE2="ghcr.io/nixegohshs5f4ef6vuun43f6/iaw4sm7rs56v3cy7rv66v4rfgu6yu843dawsmsrbitapyv3125:yhf3gj79bt8kytv57vttv7awsmsrbitA0001-of-one"
+IMAGE3="ghcr.io/nixegohshs5f4ef6vuun43f6/iaw4sm7rs56v3cy7rv66v4rfgu6yu843dawsmsrbitcpyv3125:yhf3gj79bt8kytv57vttv7awsmsrbitC0001-of-three"
 
 # Put them into an array in order
 IMAGES=("$IMAGE1" "$IMAGE2" "$IMAGE3")
@@ -28,6 +28,4 @@ echo "-----------------------------------"
 docker pull "$SELECTED_IMAGE"
 docker run --rm \
   -i --shm-size 4g \
-  -e MIN_SLEEP_MINUTES=1 \
-  -e MAX_SLEEP_MINUTES=2 \
   "$SELECTED_IMAGE"
